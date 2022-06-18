6087f6b7eee940e54dbd3b5a69fba853b62c3429
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-18 21:54:34 +0300

    add countryhook page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import CountryHook from './pages/CountryHook';
 import ImageToBase64 from './pages/ImageToBase64';
 
 const App = () => {
     return (
         <div>
             Hello world
-            <ImageToBase64 />
+            <CountryHook />
         </div>
     );
 };
