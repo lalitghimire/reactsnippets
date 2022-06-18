6d0e27457f0d89264989a2f6af7ba20fbff3cab7
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-18 21:27:19 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ImageToBase64 from './pages/ImageToBase64';
+
+const App = () => {
+    return (
+        <div>
+            Hello world
+            <ImageToBase64 />
+        </div>
+    );
+};
+
+export default App;
